63675e39b090f8081861b178406ab84f15815185
Crowdbotics <team@crowdbotics.com>
2020-05-09 14:53:18 +0000

    add package blueprint-react-native-contacts-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Contacts41600', human_name: 'Contacts', access_route: 'Contacts41600'},
 { name: 'Maps41596', human_name: 'Maps', access_route: 'Maps41596', icon: 'map'},
 { name: 'SignIn541594', human_name: 'Sign In 5', access_route: 'SignIn541594'},
 { name: 'BlankScreen41591', human_name: 'Blank Screen', access_route: 'BlankScreen41591'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Contacts41600Navigator from '../features/Contacts41600/navigator';
 import Maps41596Navigator from '../features/Maps41596/navigator';
 import SignIn541594Navigator from '../features/SignIn541594/navigator';
 import BlankScreen41591Navigator from '../features/BlankScreen41591/navigator';
@@ -24,6 +25,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+Contacts41600: { screen: Contacts41600Navigator },
 Maps41596: { screen: Maps41596Navigator },
 SignIn541594: { screen: SignIn541594Navigator },
 BlankScreen41591: { screen: BlankScreen41591Navigator },
